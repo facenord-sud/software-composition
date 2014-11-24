417129b70dd1850598dc8aa5229e46cf0515332d
facenord <facenord.sud@gmail.com>
2014-11-24 22:42:45 +0100

    add some stuff

---
+++ src/main/scala/ch/unibe/scg/dicto/test/DictoResults.scala
@@ -0,0 +1,13 @@
+package ch.unibe.scg.dicto.test
+
+import com.mashape.unirest.http.JsonNode
+
+/**
+ * Created by leo on 24.11.14.
+ */
+class DictoResults(jsonObject: JsonNode) {
+
+  def each = {
+
+  }
+}
